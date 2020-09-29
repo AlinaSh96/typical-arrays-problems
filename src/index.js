3ada6177a7890e43a8b5fa5829ac14604cc02b90
AlinaSh96 <alina30081996@gmail.com>
2020-09-29 22:08:40 +0300

    feat: end task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+
+if (!array || !array.length) return 0;
+
+  let  minNumber=array[0];
+  for (i=0;i<array.length; i++){
+    if (array[i]<minNumber){
+     minNumber = array[i];
+    }
+  else minNumber = minNumber;
+  }
+  return minNumber;
+
+  }
+  
+
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+
+ let  maxNumber=array[0];
+for (i=0;i<array.length; i++){
+  if (array[i]>maxNumber){
+   maxNumber = array[i];
+  }
+else maxNumber = maxNumber;
+}
+return maxNumber;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+  
+  let  sum=0;
+for (i=0;i<array.length; i++){
+  sum +=array[i];
+}
+let avg = sum/array.length;
+
+return avg;
 }
